79f579148cbcd491a617dbb1ed92eae49dd3e666
Thomas Stephenson <t.stephenson@cqu.edu.au>
2024-01-16 15:24:23 +1100

    changes 16/01

--- test.py
+++ test.py
@@ -1,10 +1,17 @@
+import os
 from paho.mqtt.client import Client, MQTTv311
+
+try:
+    CLIENT_USER=os.environ['MQTT_USER']
+    CLIENT_PASS=os.environ['MQTT_PASS']
+except KeyError as e:
+    raise EnvironmentError(f'{e} not bound in environment')
 
 client = Client(
     client_id="",
     protocol=MQTTv311
 )
-client.username_pw_set(username="MEL-SE", password="6CmBfJHGdHQHRaBLKaJj")
+client.username_pw_set(username=CLIENT_USER, password=CLIENT_PASS)
 
 def on_connect(client, userdata, flags, rc):
     print(f"Connected with result code {rc}")
